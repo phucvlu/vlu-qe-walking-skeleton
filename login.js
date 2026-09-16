825f5992b5de94bb232d300937e91d95d24bf7f7
Nguyen Trong Phuc <phuc.2474802010317@vanlanguni.vn>
2026-09-16 13:56:02 +0700

    Update login credentials for admin user

--- login.js
+++ login.js
@@ -5,7 +5,7 @@
  * @returns {boolean} true nếu đúng tài khoản, ngược lại false
  */
 function login(username, password) {
-  if (username === 'admin' && password === '1234') {
+  if (username === 'admin' && password === '123') {
     return true;
   }
   return false;
